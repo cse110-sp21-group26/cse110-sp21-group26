0defc631135dfcbbbfc5142d394f39866ca8459c
QinCalvin <calvin.qin.ca@gmail.com>
2021-06-03 19:34:59 -0700

    New address for live webapp added

--- source/__tests__/stickynote.test.js
+++ source/__tests__/stickynote.test.js
@@ -1,6 +1,9 @@
 describe("Sticky Note Testing ", () => {
     beforeEach(async () => {
         await page.goto("http://127.0.0.1:5500");
+        // await page.goto(
+        //     "https://cse110-sp21-group26.github.io/cse110-sp21-group26/"
+        // );
         await page.waitForTimeout(500);
         jest.setTimeout(30000);
         // await page.waitForTimeout(500000000000000000);
