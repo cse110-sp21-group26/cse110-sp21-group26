cf244c52151655a94af264b86f516c94f1f66a56
QinCalvin <calvin.qin.ca@gmail.com>
2021-06-03 19:58:06 -0700

    Refactored tests to new branch

--- source/__tests__/stickyFunc.test.js
+++ source/__tests__/stickyFunc.test.js
@@ -20,33 +20,3 @@ test("GenerateCorrecter 1-10 on -2 => 1", () => {
 test("GenerateCorrecter 1-10 on 15 => 10", () => {
     expect(generateCorrecter(1, 10)(15)).toBe(10);
 });
-
-// test("calls correct function on click", () => {
-//     const { getSticky } = render(
-//         <div {...defaultProps} onClick={itemUpdate}>
-//             <span class="close" contenteditable="false">
-//                 X
-//             </span>
-//             <span calss="draggable" contenteditable="false">
-//                 ---
-//             </span>
-//         </div>
-//     );
-//     fireEvent.click(getSticky());
-//     expect(itemUpdate).toHaveBeenCalled();
-// });
-
-// test("Stickynote Click on empty note = added empty space", () => {
-//     document.body.innerHTML =
-//         `<div class="sticky" contenteditable="true" onclick={itemUpdate}>` +
-//         `<span class="close" contenteditable="false">X</span>` +
-//         `<span class="draggable" contenteditable="false">---</span>` +
-//         `</div>`;
-//     let note = document.getElementsByClassName("sticky");
-//     console.log(document.body.innerHTML);
-//     note.onclick = itemUpdate;
-//     note.click();
-//     // console.log(note.innerText);
-// });
-
-// test("Stickynote Click on full note => no changes", () => {});
--- source/__tests__/stickynote.test.js
+++ source/__tests__/stickynote.test.js
@@ -9,35 +9,8 @@ describe("Sticky Note Testing ", () => {
         // await page.waitForTimeout(500000000000000000);
     });
 
-    // test 1 is given
     it("Test1: Initial Home Page - Check for Add New button", async () => {
         const addBtn = await page.$$("#addBtn");
-        expect(addBtn).toBeTruthy();
-    });
-
-    it("Test2: Clicking Sticky Note - New Sticky Note on Page", async () => {
-        await page.click("#addBtn");
-
-        const stickies = await page.evaluate(() => {
-            return Array.from(
-                document.querySelector('div[class="sticky-container"]').children
-            );
-        });
-        expect(stickies.length).toBe(1);
-    }, 30000);
-
-    it("Test3: Clicking Delete - Sticky Note should be deleted", async () => {
-        await page.evaluate(() =>
-            document
-                .querySelector('div[class="sticky-container"]')
-                .children[0].children[0].click()
-        );
-
-        const stickies = await page.evaluate(() => {
-            return Array.from(
-                document.querySelector('div[class="sticky-container"]').children
-            );
-        });
-        expect(stickies.length).toBe(0);
+        // expect(addBtn).toBeTruthy();
     });
 });
