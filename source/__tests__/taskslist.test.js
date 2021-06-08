be422eb6c9d8995fae37a7a5ea03b8a63337643a
yuz876 <yuz876@ucsd.edu>
2021-06-07 21:46:15 -0700

    fix bug

--- source/__tests__/taskslist.test.js
+++ source/__tests__/taskslist.test.js
@@ -1,17 +1,17 @@
 describe('Tests for taskslist ', () => {
     beforeAll(async () => {
-      await page.goto('https://cse110-sp21-group26.github.io/cse110-sp21-group26/source/index.html');      
-      await page.waitForTimeout(500);
+        await page.goto('https://cse110-sp21-group26.github.io/cse110-sp21-group26/source/index.html');      
+        await page.waitForTimeout(500);
     });
   
     
-    it('Test1: Initial Home Page - Check for tasks list module', async () => {      
-      const title = await page.$eval("#tasks_list_title", titleResult => titleResult.textContent);
-      expect(title).toBe("Tasks List");
+    it('Test1: Initial Home Page - Check for tasks list module', async () => {   
+        const title = await page.$eval("#tasks_list_title", titleResult => titleResult.textContent);
+        expect(title).toContain("Tasks List");
     });
 
     it('Test2: Initial Home Page - Check for tasks list form', async () => {      
-        const formCN = await page.$eval("#tasks_list_form", formResult => titleResult.className);
+        const formCN = await page.$eval("#tasks_list_form", formResult => formResult.className);
         expect(formCN).toBe("tasks_list_form");
-      });
+    });
 });
