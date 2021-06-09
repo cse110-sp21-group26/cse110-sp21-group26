161e75ada0bc807d231192f9d5664c6d25f01ad7
Josh <jsantill@ucsd.edu>
2021-06-09 02:41:48 -0700

    fix Margins on skeleton, added JSDOC

--- source/scripts/moodScript.js
+++ source/scripts/moodScript.js
@@ -1,11 +1,21 @@
+//==============
+// State Variables
 let getDot = 0;
 var motValidation = false;
 var strValidation = false;
 var moodClickedValidation = false;
 var getMotLevel;
 var getStrLevel;
+//===============
 
-
+/**
+ * Validates a button has been pressed
+ * Makes clicked emoji bigger and resets others to normal scale.
+ * Presents correct feeling paragraph underneath.
+ * Checks for form completion
+ * @param {string} e 
+ * @param {string} id 
+ */
 function moodClicked(e,id){
     moodClickedValidation = true;
  
@@ -18,17 +28,24 @@ function moodClicked(e,id){
     document.getElementById(id).style.transform = "scale(1.4)"; 
     document.getElementById(id).style.textShadow = "10px 10px #ff0000";
 
-    showMood.textContent = "You feel " + e +"."
+    showMood.textContent = "You feel " + e +"." 
     showMood.style.display = 'block';    
     btnDisplay();
 }
-
+/**
+ * Checks button requirements
+ * if all validaitons are true button will display
+ */
 function btnDisplay(){
     let btnToggle = document.getElementById('moodBtn');
     if(strValidation && motValidation && moodClickedValidation)
         btnToggle.style.display = 'block';
 }
-
+/**
+ * Passes a number through to display the background on the respective clicked number
+ * clears recently clicked colors
+ * @param {Integer} e 
+ */
 function dotColorOnCLickMot(e) {
     for(let I = 1; I <= 5; I++)
         document.getElementById("motdot"+I).style.background = "#FFFFFF"; 
@@ -37,7 +54,11 @@ function dotColorOnCLickMot(e) {
     motValidation = true;
     btnDisplay();
 } 
-
+/**
+ * Passes a number through to display the background on the respective clicked number
+ * clears recently clicked colors
+ * @param {Integer} e 
+ */
 function dotColorOnCLickStr(e) {
     for(let I = 1; I <= 5; I++)
         document.getElementById("strdot"+I).style.background = "#FFFFFF";
@@ -47,7 +68,9 @@ function dotColorOnCLickStr(e) {
 
     btnDisplay();
 } 
-
+/**
+ * Used for localStorage
+ */
 function storeData(){
     console.log(getStrLevel + " " + getMotLevel);
 }
